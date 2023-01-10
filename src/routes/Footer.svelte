5fad3658431bd2600edab5b82d1673a6b8393beb
dylandupasquier <dylandupasquier@gmail.com>
2023-01-09 21:11:21 -0800

    coffee new tab

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,7 +4,7 @@
 		Stories We Tell - &copy; {new Date().getFullYear()}
 	</div>
 	<div class="right">
-		<a href="https://www.buymeacoffee.com/dylandupasquier"
+		<a href="https://www.buymeacoffee.com/dylandupasquier" target="_blank" rel="noopener noreferrer"
 			><img
 				src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=dylandupasquier&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
 				alt="buy-me-a-coffee"
