a555fe4adee63ae3fbc927132682770d628975fa
dylandupasquier <dylandupasquier@gmail.com>
2023-01-28 22:12:48 -0800

    fix

--- src/routes/story/view/[story_id]/+page.svelte
+++ src/routes/story/view/[story_id]/+page.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-	
-	import CanvasView from '../CanvasView.svelte';
-	import { getPageThumbnail } from '$lib/services/getImages';
-
-	export let data: ViewPageProps;
-	// $: console.log(data)
-	
-	let screenshot: { publicUrl: string } | undefined;
-
-	
-
-	// $: if (data) {
-	// 	getPageThumbnail(data.page[0].screenshot).then((res) => {
-	// 		screenshot = res;
-	// 	});
-	// }
+import CanvasView from '../CanvasView.svelte';
 </script>
 
-<CanvasView screenshot={screenshot?.publicUrl}/>
+<CanvasView />
