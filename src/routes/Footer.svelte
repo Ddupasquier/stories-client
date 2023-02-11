32e853dc2b6b86ba53a5057164875fdd73e99060
dylandupasquier <dylandupasquier@gmail.com>
2023-02-10 20:05:37 -0800

    whatever

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-	// import {onMount} from 'svelte';
 	import { domainName } from '../lib/constants';
-	
-
-// 	onMount(() => {
-//       (window.adsbygoogle = window.adsbygoogle || []).push({});
-//     });
-	// import {githubIcon} from '$lib/assets';
 </script>
 
-<!-- <amp-ad
-     layout="fixed"
-     width="728"
-     height="90"
-     type="adsense"
-     data-ad-client="ca-pub-3560491105009054"
-     data-ad-slot="8764240828">
-</amp-ad> -->
 <footer>
 	<div class="left">
-		<!-- <a href="https://github.com/Ddupasquier/stories-client" target="_blank" rel="noopener noreferrer">
-			<img src={githubIcon} alt="github" />
-		</a> -->
 	</div>
 	<div class="center">
 		{domainName} - &copy; {new Date().getFullYear()}
