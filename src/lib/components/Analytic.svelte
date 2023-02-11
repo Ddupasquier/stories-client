5b74005d6799f55dd86cf722944f462b31896deb
dylandupasquier <dylandupasquier@gmail.com>
2023-02-10 19:32:12 -0800

    maybe ads?

--- src/lib/components/Analytic.svelte
+++ src/lib/components/Analytic.svelte
@@ -2,9 +2,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	const analyticsId = import.meta.env.VITE_ANALTYICS_ID;
+	const googleAds = import.meta.env.VITE_ADSENSE_ID;
+
 	$: {
 		if (typeof gtag !== 'undefined') {
-			gtag('config', 'G-HYH2C42YHT', {
+			gtag('config', analyticsId, {
 				page_title: document.title,
 				page_path: $page.url.pathname
 			});
@@ -25,4 +28,8 @@
 		gtag('js', new Date());
 		gtag('config', 'MEASUREMENT_ID');
 	</script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client={googleAds}"
+     crossorigin="anonymous">
+
+</script>
 </svelte:head>
